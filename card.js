d346ec0b1265635c1f7af2189bafc5434a57ee52
Teila A Garraway <tgarraway08@yahoo.com>
2021-09-23 23:41:36 -0400

    finished

--- card.js
+++ card.js
@@ -7,12 +7,22 @@ const references = [
   {
     Name: "Andrea Dobur - State Manager",
     Comment:
-      "I am pleased to recommend Teila for a software development position. Teila is very organized, detailed orientated, and shows great leadership. For example, she has trained new employees on how to be a Legal Assistant, and effective at their jobs. She created a manual that her co-workers use as a reference to do the many tasks. She has also assisted with the on-boarding of new Attorney hires, training them on how to comply with Progressive’s security policies, how to use both the local and virtual desktops, and how to be more efficient. Her IT expertise has been tremendously valuable not only to her peers,but to the entire GA HC mgmt team arnd state by poviding training regarding the various IT issues we run into. We call her training sessions Teila’s Tech Tip Tuesdays. I really enjoy working with Teila.  She has brought so much to the GA HC team. Please let me know if you need any additional information.",
+      "I am pleased to recommend Teila for a software development position.Teila is very organized, detailed orientated, and shows great leadership.For example, she has trained new employees on how to be a Legal Assistant, and effective at their jobs. She created a manual that her co-workers use as a reference to do the many tasks. She has also assisted with the on-boarding of new Attorney hires, training them on how to comply with Progressive’s security policies, how to use both the local and virtual desktops, and how to be more efficient. Her IT expertise has been tremendously valuable not only to her peers,but to the entire GA HC mgmt team arnd state by poviding training regarding the various IT issues we run into. We call her training sessions Teila’s Tech Tip Tuesdays. I really enjoy working with Teila. She has brought so much to the GA HC team. Please let me know if you need any additional information.",
   },
   {
     Name: "Harvey Beress - Attorney Manager",
     Comment:
       "I think you would be great in a technology role, or in a variety of fields outside of legal. You are one of the top Legal Assistants in one of Progressive’s largest and highest volume house counsel offices in the country.You thrive in an environment that requires you to multi-task, to work quickly and to be very accurate. You go above and beyond on a regular basis, not just by assisting other LAs, but also by creating your “Teila’s Tech Tip” series, and even taking your valuable time to give me a one-on-one tech training session.  Your manager has the trust and confidence in you to have you train new attorneys on the ins and outs of our case management software. You are an integral part of how we try to maintain an excellent work environment.You are smart, hard-working and capable of doing anything you set your mind to.You would be a great asset to the Technology Department at Progressive.",
+  },
+  {
+    Name: "Dodson Strawbridge - Attorney II",
+    Comment:
+      "Adaptability. That is my biggest single take-away from my 4 years paired with Teila in Georgia House Counsel. When we became partners she was relatively new to the Legal Assistant role and we were both totally new to each other. Throughout our time together, she repeatedly demonstrated the ability to adapt to unusual circumstances. Whether it was implementing a new internal process, executing a novel litigation strategy or simply tolerating my unique preferences, Teila’s ability to adapt and succeed under constantly evolving circumstances made her an invaluable member of my team in particular, and Georgia House Counsel in general. You’d be lucky to have her join your team!",
+  },
+  {
+    Name: "Karolynn Harris - Attorney II",
+    Comment:
+      "Teila has worked with me, side by side, as my Legal Assistant for 2 years.  Prior to that, I had the opportunity to observe her strong work ethic in our work place.  Teila is a people person that gets along well with her teammates.  She is a self-starter, who excels both in an individual and group settings.  Since the pandemic, she has not missed a beat adapting to the 100% remote environment. She has excellent communication and time-management skills, which are key to any business.   I find it most helpful that if Teila sees a problem or a process that lacks efficiency, she is the first one to offer logical and well thought out solutions. She has developed processes in our office that help our jobs run smoother. She is a great person and an absolute asset to my team. ",
   },
   {
     Name: "Kendall Baker - Attorney II",
@@ -25,11 +35,20 @@ const references = [
       "Overall Teila Garraway is great co-worker and a fantastic friend. Teila is always willing to help with her co-workers’ workload if need be.  She’s volunteered to go into the office to help mail documents since the beginning of the pandemic.  It also helps that Teila is very knowledgeable about computers and the programs we use here in House Counsel.  She even hosts a Tech Tip Tuesday with Teila where she discusses and instructs her peers on different computer / program related issues.  I met Teila four years ago.  Teila was very standoffish, however, I was determined to be her friend.  I chipped away at her rough exterior until I wore her down and we’ve been friends ever since. If you have the pleasure of hiring Teila, you won’t regret it. ",
   },
   {
+    Name: "Jami Mencer - Previous SS Supervisor /Current IT Assoc Project Mngr Sr",
+    Comment:
+      "I have known Teila almost my entire career with Progressive. One thing that has always been consistent is growth. She has always been ready to learn and master a new skill. After she has learned a new skill, she is already ready to share that information with her co-workers to help them grow. If she joins your team, she will work hard to grow her skills and ensure that others can learn as well.",
+  },
+  {
+    Name: "Cheryl Maltbie - Legal Assistant II (Lead)",
+    Comment:
+      "Teila is a hard worker and willing to help out in any area needed.  Teila is conscientious and takes pride in her work, she has also helped onboard legal assistants along with new attorneys showing them the ropes in working with our computer system. Teila has also had team meetings showing everyone tips to navigate the computer.",
+  },
+  {
     Name: "Denise Cummings - Admisnistrative Assistant (Lead)",
     Comment:
       "Overall Teila Garraway is great co-worker and a fantastic friend. Teila is always willing to help with her co-workers’ workload if need be.  She’s volunteered to go into the office to help mail documents since the beginning of the pandemic.  It also helps that Teila is very knowledgeable about computers and the programs we use here in House Counsel.  She even hosts a Tech Tip Tuesday with Teila where she discusses and instructs her peers on different computer / program related issues.  I met Teila four years ago.  Teila was very standoffish, however, I was determined to be her friend.  I chipped away at her rough exterior until I wore her down and we’ve been friends ever since. If you have the pleasure of hiring Teila, you won’t regret it. ",
   },
-
   {
     Name: "Janelle Bowden - Legal Assistant I",
     Comment:
@@ -39,11 +58,6 @@ const references = [
     Name: "Betsabe Hernandez - Legal Assistant II",
     Comment:
       "Teila plays a crucial role in HC success. Teila is obvious how she pays attention to detail. Teila is an awesome employee!!  Teila is an easy-going person that makes work enjoyable. Teila is always open-minded and willing to consider all possible options. She has creative ideas helps us generate better, more innovative ideas for our coworkers. She has started to teach the employees easier ways about technology and making sure everyone understand it.",
-  },
-  {
-    Name: "Cheryl Maltbie - Legal Assistant II (Lead)",
-    Comment:
-      "Teila is a hard worker and willing to help out in any area needed.  Teila is conscientious and takes pride in her work, she has also helped onboard legal assistants along with new attorneys showing them the ropes in working with our computer system. Teila has also had team meetings showing everyone tips to navigate the computer.",
   },
   {
     Name: "Theresa Larry - Legal Assistant II ",
@@ -56,12 +70,6 @@ const references = [
       "Teila is an amazing resource at House Counsel. She singlehandedly started a program called Tech Tip Tuesdays. Through Teams, she answers any technical question we have after she covered the topics that were on the agenda for the day.",
   },
   {
-    Name: "Jeffrey V Brand -  Litigation Specialist ",
-    Comment:
-      "First off, and this is me being selfish, I do NOT want you to leave your current position 😊 But with all seriousness, where you work will have absolutely no bearing on your job performance. You are definitely not the type of person that needs to be baby sat.You take pride in your work and it shows. You are a self-starter, quick learner, and a person that follows through.There has never been one time that I’ve reached out to you with a question or need that you didn’t respond back to me immediately. Not 24 hours later. Immediately.And aside from your excellent job performance you ALWAYS have an upbeat and positive attitude! Proof..Even after this past year, with Covid, my inventory alone running 250 features (I cannot imagine the case load on the defense attorney side that you had to deal with)… Teila, that speaks volumes and clearly shows you can handle anything.",
-  },
-
-  {
     Name: "Nan Fisher - Legal Assitant II",
     Comment:
       "Teila is my always go to whenever I have any technology issues.  She always responds quickly and always has the way to either fix an issue or tips for making technology more efficient and user friendly.  She makes sure to send pictures of any steps needed for the entire process, beginning to end.  As a co-worker, she can always be depended on to help others out, answer any questions you may have and will give suggestions to make the job easier.  She has prepared an entire One Note that has helped everyone with great tips on absolutely everything. As a person, she is a kind, helpful, always coming up with new ideas, has the best personality ever and is a personal ray of sunshine in my life. You go girl!  I wish you all the luck in this coming through for you.  You certainly deserve it!  Let me know if there is anything else you need from me. ",
@@ -70,6 +78,11 @@ const references = [
     Name: "Terranique Grice - Administrative Assistant",
     Comment:
       "To whom this may concern:  I’ve been with Progressive House Counsel for over 3 years and witness Teila hard-work, leadership and expertise.  She is not afraid to step up and lead a group whether it’s a meeting, community service, assisting in other areas or tech issues. I know she is capable and has the skills to be a Programming Developer, she has helped our team with many technical issues or concerns.  Last, I am proud of her recent achievement obtaining a certificate in Coding.   This shows how dedicated and passionate she is about the field. I know she will do extraordinary whenever she is because she has the mind-set to achieve any accomplishments pursued.",
+  },
+  {
+    Name: "Jeffrey V Brand -  Litigation Specialist ",
+    Comment:
+      "First off, and this is me being selfish, I do NOT want you to leave your current position 😊 But with all seriousness, where you work will have absolutely no bearing on your job performance. You are definitely not the type of person that needs to be baby sat.You take pride in your work and it shows. You are a self-starter, quick learner, and a person that follows through.There has never been one time that I’ve reached out to you with a question or need that you didn’t respond back to me immediately. Not 24 hours later. Immediately.And aside from your excellent job performance you ALWAYS have an upbeat and positive attitude! Proof..Even after this past year, with Covid, my inventory alone running 250 features (I cannot imagine the case load on the defense attorney side that you had to deal with)… Teila, that speaks volumes and clearly shows you can handle anything.",
   },
 ];
 window.addEventListener("DOMContentLoaded", (event) => {
